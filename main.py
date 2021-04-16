5a83723a2b554bc22c6af434572b84591df73cae
Sally <sally_zhi@brown.edu>
2021-04-16 14:18:05 -0400

    pics and ranges

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def checkInRange(colors, curr_pixel):
     print("not in any color range")
 
 def edgeDetection():
-    imgobj = cv2.imread('better_lighting/face1.2.jpg')
+    imgobj = cv2.imread('better_lighting/face6.3.jpg')
     gray = cv2.cvtColor(imgobj, cv2.COLOR_BGR2GRAY)
     cv2.namedWindow("image")
     blurred = cv2.GaussianBlur(gray, (3,3), 0)
@@ -88,7 +88,7 @@ def sobel(image):
 
 
 def detectColors():
-    image = cv2.imread('better_lighting/face1.2.jpg')
+    image = cv2.imread('better_lighting/face6.3.jpg')
     original = image.copy()
 
     image = remove_background(image)
@@ -114,9 +114,10 @@ def detectColors():
         # 'gray': ([76, 0, 41], [179, 255, 70]),        # Gray
         'blue': ([90, 50, 50], [130, 255, 255]),
         'yellow': ([20, 100, 100], [40, 255, 255]),   # Yellow
-        'orange': ([0, 50, 50], [30, 255, 255]),     # Orange
+        'orange': ([8, 80, 80], [30, 255, 255]),     # Orange
         'green' : ([60 - 14, 100, 100], [60 + 20, 255, 255]), # Green
-        'red' : ([159, 50, 70], [180, 255, 255]), #Red
+        'red' : ([159, 50, 50], [180, 255, 255]), #Red
+        'red' : ([0, 70, 50], [8, 255, 255]), #Red
         'white' : ([0,0,210], [255,255,255]) # White
         }
 
