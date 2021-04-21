d45a382ff380ae4d8dc0421558e76a04c91c4ace
Sally <sally_zhi@brown.edu>
2021-04-21 17:42:35 -0400

    commenting video.py

--- video.py
+++ video.py
@@ -2,50 +2,46 @@ import cv2
 import numpy as np
 from imutils import contours
 import segmentation
-from main import checkInRange
 import color_kmeans
 
-# colors = {
-#         # 'gray': ([76, 0, 41], [179, 255, 70]),        # Gray
-#         'blue': ([90, 50, 70], [130, 255, 255]),
-#         'yellow': ([20, 100, 100], [40, 255, 255]),   # Yellow
-#         'orange': ([0, 50, 50], [30, 255, 255]),     # Orange
-#         'green' : ([60 - 14, 100, 100], [60 + 20, 255, 255]), # Green
-#         'red' : ([159, 50, 70], [180, 255, 255]), #Red
-#         'white' : ([0,0,230], [255,255,255]) # White
-#         }
+'''
+Check if pixel is in any of the color ranges
+'''
+def checkInRange(colors, curr_pixel):
+    for key, value in colors.items():
+        lower_bound = value[0]
+        upper_bound = value[1]
+        if (curr_pixel[0] >= lower_bound[0] and curr_pixel[0] <= upper_bound[0] and curr_pixel[1] >= lower_bound[1] and curr_pixel[1] <= upper_bound[1] and  curr_pixel[2] >= lower_bound[2] and curr_pixel[2] <= upper_bound[2]):
+            return key
+    print("not in any color range")
+
+# color ranges
 colors = {
-        # 'gray': ([76, 0, 41], [179, 255, 70]),        # Gray
-        'blue': ([90, 50, 70], [128, 255, 255]),
+        'blue': ([90, 50, 70], [128, 255, 255]), # Blue
         'yellow': ([21, 110, 117], [45, 255, 255]),   # Yellow
         'red' : ([0, 100, 100], [8, 255, 255]), #Red,
         'red2' : ([159, 50, 70], [180, 255, 255]), #Red
         'orange': ([0, 110, 125], [17, 255, 255]),     # Orange
         'green' : ([60 - 20, 100, 100], [60 + 20, 255, 255]), # Green
-        # 'white' : ([0,0,1], [0,0,255]) # White
-        'white': ([0,0,210], [255,255,255])
+        'white': ([0,0,210], [255,255,255]) # White
         }
 
-faces = ["front", "right", "back", "left", "top", "bottom"]
-face_index = 0
+
+# define webcam
 webcam = cv2.VideoCapture(0)
 cube_state = []
 while True:
+    # get frame
     _, image = webcam.read()
     original = image.copy()
     key = cv2.waitKey(10) & 0xFF
 
+    # define bounding box in which to place cube
     box_start = (image.shape[1] // 2 - image.shape[1] // 6, image.shape[0] // 2 - image.shape[1] // 6)
-
-    cv2.rectangle(original, box_start, (box_start[0] + image.shape[1] // 3, box_start[1] + image.shape[1] // 3), (36,255,12), 2)
-        
+    cv2.rectangle(original, box_start, (box_start[0] + image.shape[1] // 3, box_start[1] + image.shape[1] // 3), (36,255,12), 2)     
     cropped_cube = image[box_start[1]:box_start[1]+image.shape[1] // 3, box_start[0]:box_start[0] + image.shape[1] // 3]
-
     cropped_cube = segmentation.remove_background(cropped_cube)
-
     cropped_cube = cv2.cvtColor(cropped_cube, cv2.COLOR_BGR2HSV)
-    # cv2.imshow('hsv', image)
-    # print(image[176, 774])
     mask = np.zeros(cropped_cube.shape, dtype=np.uint8)
 
     # Color threshold to find the squares
@@ -69,6 +65,7 @@ while True:
     # Sort all contours from top-to-bottom or bottom-to-top
     (cnts, _) = contours.sort_contours(cnts, method="top-to-bottom")
 
+    # remove contours below certain size limit
     new_cnts = []
     for contour in cnts:
         x, y, w, h = cv2.boundingRect(contour)
@@ -81,7 +78,6 @@ while True:
 
     for (i, c) in enumerate(cnts, 1):
         x,y,w,h = cv2.boundingRect(c)
-        # if (w*h >= 2000):
         row.append(c)
         if i % 3 == 0:  
             (cnts, _) = contours.sort_contours(row, method="left-to-right")
@@ -101,24 +97,15 @@ while True:
 
                 if (curr_x >= 0 and curr_x <= image.shape[1] and curr_y >= 0 and curr_y <= image.shape[0]):
                     curr_color = checkInRange(colors, image[curr_y][curr_x])
-                    # curr_color = checkInRange(colors, curr)
                 string = str(curr_color ) + " " + str("#{}".format(number + 1))
                 cv2.putText(original, string, (x+box_start[0],y+box_start[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
                 number += 1
 
+    # press q to exit webcam
     if key == ord('q'):
         webcam.release()
         cv2.destroyAllWindows()
         break
-
-    if key == ord('s'):
-        face_index += 1
-        if face_index == 6:
-            webcam.release()
-            cv2.destroyAllWindows()
-            break
-    else:
-        cv2.putText(original, "Press s to capture " + faces[face_index] + " face", (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
 
     cv2.imshow("Real Time Color Detection", original)
     
