4412101380368625884ac3f686424c557441b507
Lucy Qu <lucyqu@Lucys-MacBook-Pro.local>
2021-04-21 13:36:41 -0400

    read from folder

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from imutils import contours
 import segmentation
 from color_kmeans import classify_colors, match_color
 import kociemba as kc
+import argparse
+import os
 
 kociemba = {
         'BLUE': 'F',
@@ -33,34 +35,31 @@ def main():
 
     cube_state = []
 
-    # parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-    # parser.add_argument('-i', '--images', help='File path to images folder')
-    # args = parser.parse_args()
-    # image_folder = args.images
+    parser.add_argument('-i', '--images', help='File path to images folder')
+    args = parser.parse_args()
+    image_folder = args.images
 
-    # for filename in sorted(os.listdir(image_folder)):
-    #     if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"):
-    #         path = os.path.join(image_folder, filename)
-    #         face_state = detectColors(path, img_number)
-    #         cube_state.extend(face_state)
+    face_number = 1
 
-    for i in range(6):
-        pic = str(i + 1)
-        image = cv2.imread('cube2/face' + pic + '.jpeg')
-        cropped_cube = segmentation.corner_detection(image)
-        dims = 3
-        color_lst = []
+    for filename in sorted(os.listdir(image_folder)):
+        if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"):
+            path = os.path.join(image_folder, filename)
+            image = cv2.imread(path)
+            cropped_cube = segmentation.corner_detection(image)
+            dims = 3
+            color_lst = []
 
-        for r in range(dims):
-            for c in range(dims):
-                square = segmentation.get_square(cropped_cube, dim=dims, row=r, col=c)
-                rgb_colors = classify_colors(square, 2, show_chart=False)
-                matched_color = match_color(rgb_colors)
-                color_lst.append(matched_color)
-                cube_state.append(matched_color)
-        
-        print('Face ' + pic + ': ', color_lst)
+            for r in range(dims):
+                for c in range(dims):
+                    square = segmentation.get_square(cropped_cube, dim=dims, row=r, col=c)
+                    rgb_colors = classify_colors(square, 2, show_chart=False)
+                    matched_color = match_color(rgb_colors)
+                    color_lst.append(matched_color)
+                    cube_state.append(matched_color)
+            print('Face ' + str(face_number) + ': ', color_lst)
+            face_number += 1
     
     cube_state_string = getKociembaString(cube_state)
     if (len(cube_state_string) != 54):
